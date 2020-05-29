0df4d684e2a7feda60c8b67c7aac403da956d45e
zsimo <zsimo30@gmail.com>
2020-05-29 20:57:42 +0000

    improve groupConsumer

--- stream/groupConsumer.js
+++ stream/groupConsumer.js
@@ -14,51 +14,58 @@ const RESPONSE = 1;
 const KEY = 0;
 const VALUE = 1;
 
+var errorCounter = 0;
+
 const { promisify } = require("util");
 // promisify redis functions in order to use async/await sintax
 const XACK = promisify(client.XACK).bind(client);
 const XDEL = promisify(client.XDEL).bind(client);
 
-/**
- * All the pending (not yet processed) items will be delivered to all the running workers, without any duplication.
- */
-function consume () {
+// consume every previous unprocessed items (if any)
+client.XREADGROUP ("GROUP", config.group_name, uniqueConsumerName, "BLOCK", 0, "STREAMS", config.stream_name, 0, _process);
 
+// consume every new items
+function consume () {
     // 0 means block forever until a new item
     // 0 means give me everything from beginning or ">" receive only messages that were never delivered to any other consumer
-    client.XREADGROUP ("GROUP", config.group_name, uniqueConsumerName, "BLOCK", 0, "STREAMS", config.stream_name, ">", async function (err, reply) {
-        try {
+    client.XREADGROUP ("GROUP", config.group_name, uniqueConsumerName, "BLOCK", 0, "STREAMS", config.stream_name, ">", _process);
+}
+
+// do something with the item pulled out from the stream
+async function _process (err, reply) {
+    try {
             
-            var items = reply[0][1];
+        var items = reply[0][1];
 
-            for (var i = 0; i < items.length; i += 1) {
-                var item = items[i];
-                var id = item[ID];
-                var response = item[RESPONSE];
-                var key = response[KEY];
-                var value = response[VALUE];
+        for (var i = 0; i < items.length; i += 1) {
+            var item = items[i];
+            var id = item[ID];
+            var response = item[RESPONSE];
+            var key = response[KEY];
+            var value = response[VALUE];
 
-                // do some operation with the value...
-                console.log("value from the stream", value);
+            // do some operation with the value...
+            console.log("value from the stream", value);
 
-                //throw new Error("ciao");
-                // then tell redis that the item has been processed
-                var xackResult = await XACK(config.stream_name, config.group_name, id);
+            // throw new Error("ciao");
+            // then tell redis that the item has been processed
+            var xackResult = await XACK(config.stream_name, config.group_name, id);
 
-                // and finally remove the item from the stream
-                var xdelResult = await XDEL(config.stream_name, id);
-                
-            }
+            // and finally remove the item from the stream
+            var xdelResult = await XDEL(config.stream_name, id);
+            
+        }
 
-
-        } catch (e) {
-            console.error(e);
-         }
-
-         consume();
-        
-    });
-    
+    } catch (e) {
+        errorCounter += 1;
+        // in case of too many errors (e.g.: redis-cli flushall) exit the process
+        // and let pm2 to reload it
+        if (errorCounter > 1000) {
+            process.exit();
+        }
+     }
+     consume();
+   
 }
 
 client.XGROUP("CREATE", config.stream_name, config.group_name, 0, function (err, reply) {
