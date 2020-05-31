61ca0a1a0e85fa8db46f34e95c38cce22e69fe48
zsimo <zsimo30@gmail.com>
2020-05-31 12:38:58 +0000

    improve groupConsumer

--- stream/groupConsumer.js
+++ stream/groupConsumer.js
@@ -2,38 +2,72 @@
 
 var args = process.argv.slice(2);
 var path = require("path");
+var fs = require("fs");
 var client = require(path.resolve(process.cwd(), "redis"));
 var config = require(path.resolve(__dirname, "config"));
 var uniqueConsumerName = args[0];
 if (!uniqueConsumerName) {
     throw new Error("consumer name is mandatory");
 }
+var error = false;
+var timeoutId;
 
 const ID = 0;
 const RESPONSE = 1;
 const KEY = 0;
 const VALUE = 1;
+const RETRY_TIMEOUT = 5000; // in ms
 
-var errorCounter = 0;
+var DELIVERY_STRATEGIES = Object.freeze({
+    PENDING: 0,
+    NEW: ">" // never delivered
+});
 
 const { promisify } = require("util");
 // promisify redis functions in order to use async/await sintax
 const XACK = promisify(client.XACK).bind(client);
 const XDEL = promisify(client.XDEL).bind(client);
+const XPENDING = promisify(client.XPENDING).bind(client);
+const XREADGROUP = promisify(client.XREADGROUP).bind(client);
 
-// consume every previous unprocessed items (if any)
-client.XREADGROUP ("GROUP", config.group_name, uniqueConsumerName, "BLOCK", 0, "STREAMS", config.stream_name, 0, _process);
+async function consume () {
 
-// consume every new items
-function consume () {
-    // 0 means block forever until a new item
-    // 0 means give me everything from beginning or ">" receive only messages that were never delivered to any other consumer
-    client.XREADGROUP ("GROUP", config.group_name, uniqueConsumerName, "BLOCK", 0, "STREAMS", config.stream_name, ">", _process);
+    try {
+
+        var strategy = DELIVERY_STRATEGIES.NEW;
+        var pending = await XPENDING(config.stream_name, config.group_name);
+
+        var numberOfPending = pending[0];
+        console.log("pending " + numberOfPending);
+        /**
+         * Consume all the pending messages (if any) and then the BLOCK waiting for the new ones.
+         * @see https://redis.io/commands/xreadgroup last chapter
+         */
+        if (numberOfPending > 0) {
+            strategy = DELIVERY_STRATEGIES.PENDING;
+        }
+
+        // "BLOCK", 0 means block forever until a new item (if the strategy = ">", otherwise BLOCK will be ignored)
+        client.XREADGROUP("GROUP", config.group_name, uniqueConsumerName, "BLOCK", 0, "STREAMS", config.stream_name, strategy, _readTheStream);
+
+    } catch(e) {
+
+        // create the group if not exists
+        client.XGROUP("CREATE", config.stream_name, config.group_name, 0, function (err, reply) {
+            console.log("group created",  config.group_name, reply);
+        });
+        
+    }
+
 }
 
 // do something with the item pulled out from the stream
-async function _process (err, reply) {
+async function _readTheStream (err, reply) {
     try {
+
+        if (error === true) {
+            return;
+        }
             
         var items = reply[0][1];
 
@@ -46,31 +80,32 @@ async function _process (err, reply) {
 
             // do some operation with the value...
             console.log("value from the stream", value);
+            fs.readFileSync(path.resolve(__dirname, "test.txt"));
 
-            // throw new Error("ciao");
             // then tell redis that the item has been processed
             var xackResult = await XACK(config.stream_name, config.group_name, id);
 
             // and finally remove the item from the stream
             var xdelResult = await XDEL(config.stream_name, id);
             
+            error = false;
+            clearTimeout(timeoutId);
         }
 
     } catch (e) {
-        errorCounter += 1;
-        // in case of too many errors (e.g.: redis-cli flushall) exit the process
-        // and let pm2 to reload it
-        if (errorCounter > 1000) {
-            process.exit();
-        }
+        console.error(e);
+        error = true;
+        clearTimeout(timeoutId);
+        timeoutId = setTimeout(function () {
+            error = false;
+            consume();
+        }, RETRY_TIMEOUT);
+
      }
+
      consume();
    
 }
-
-client.XGROUP("CREATE", config.stream_name, config.group_name, 0, function (err, reply) {
-    console.log(err, reply);
-});
 
 
 consume();
--- stream/producer.js
+++ stream/producer.js
@@ -6,23 +6,29 @@ var config = require(path.resolve(__dirname, "config"));
 var count = 0;
 
 
-var count = 0;
-
 setInterval(function () {
+
+    client.XPENDING(config.stream_name, config.group_name, function (err, reply) {
+        console.log("pending: ", reply[0]);
+    });
+    
+    client.XLEN(config.stream_name, function (err, reply) {
+        console.log("total", reply);
+    });
+
     // * means autogenerate the id
     // then key, values pair
     client.XADD(config.stream_name, "*", "count", count, function (err, reply) {
-        console.log(err, reply);
-    });
-
-
-    client.XLEN(config.stream_name, function (err, reply) {
-        console.log(err, reply);
+        console.log("new added id", reply, "value", count);
     });
 
     count ++
 }, 1000);
 
+
+client.XGROUP("CREATE", config.stream_name, config.group_name, 0, function (err, reply) {
+    console.log(err, reply);
+});
 
 /** 
 client.XTRIM(config.stream_name, "MAXLEN", 10, function (err, reply) {
@@ -30,6 +36,3 @@ client.XTRIM(config.stream_name, "MAXLEN", 10, function (err, reply) {
 });
 */
 
-client.XPENDING(config.stream_name, config.group_name, function (err, reply) {
-    console.log(err, reply);
-});
